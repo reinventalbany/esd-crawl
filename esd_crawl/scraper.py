ad5244ff8726387badfde1d74d90ed45d46202db
Aidan Feldman <aidan.feldman@gmail.com>
2022-10-05 03:02:45 -0400

    download PDFs in scraper

--- esd_crawl/scraper.py
+++ esd_crawl/scraper.py
@@ -9,7 +9,10 @@ class ESDSpider(SitemapSpider):
     # https://docs.scrapy.org/en/latest/topics/settings.html#built-in-settings-reference
     custom_settings = {
         # throttle the crawl to reduce load on the site
-        "DOWNLOAD_DELAY": 0.25
+        "DOWNLOAD_DELAY": 0.25,
+        # https://docs.scrapy.org/en/latest/topics/media-pipeline.html#enabling-your-media-pipeline
+        "ITEM_PIPELINES": {"scrapy.pipelines.files.FilesPipeline": 1},
+        "FILES_STORE": "downloads",
     }
 
     def parse(self, response):
@@ -18,7 +21,8 @@ class ESDSpider(SitemapSpider):
             title = link.css("::text").get()
             url = link.css("::attr(href)").get()
 
-            yield {"title": title, "url": url, "source": response.url}
+            # https://docs.scrapy.org/en/latest/topics/media-pipeline.html#using-the-files-pipeline
+            yield {"title": title, "file_urls": [url], "source": response.url}
 
         # for next_page in response.css("a[href]::attr(href)").extract():
         #     yield response.follow(next_page, self.parse)
