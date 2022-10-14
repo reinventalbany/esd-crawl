522040e9ba22481bcf4ab1925b5dde9fc77641f7
Aidan Feldman <aidan.feldman@gmail.com>
2022-10-14 02:53:00 -0400

    create PDF records in Airtable

--- esd_crawl/airtable.py
+++ esd_crawl/airtable.py
@@ -15,22 +15,41 @@ def create_record(table_name, record):
     headers = {"Authorization": f"Bearer {key}"}
     data = {"records": [record]}
 
-    return requests.post(base, headers=headers, json=data)
+    response = requests.post(base, headers=headers, json=data)
+    resp_data = response.json()
+
+    if response.status_code != 200:
+        # failure
+        print("")
+        print(resp_data)
+        return None
+
+    return resp_data["records"][0]["id"]
+
+
+def create_pdf(url, title):
+    record = {"fields": {"URL": url, "Titles": title}}
+    return create_record("PDFs", record)
+
+
+def create_table(img_url, pdf_id):
+    record = {
+        "fields": {
+            "Image": [{"url": img_url}],
+            "PDF": [pdf_id],
+        }
+    }
+    return create_record("Tables", record)
 
 
 for pdf in pdfs:
+    pdf_url = pdf["file_urls"][0]
+    pdf_id = create_pdf(pdf_url, pdf["title"])
+    if pdf_id is None:
+        continue
+
     for image in pdf["img_paths"]:
         img_url = img_prefix + image
-        record = {
-            "fields": {
-                "Image": [{"url": img_url}],
-                "PDF": [],
-            }
-        }
-
-        response = create_record("Tables", record)
-        if response.status_code == 200:
+        table_id = create_table(img_url, pdf_id)
+        if table_id:
             print(".", end="", flush=True)
-        else:
-            print("")
-            print(response.json())
