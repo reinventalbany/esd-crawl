0b47e1055ff22fb70ddad1768b0e94ad21e31216
Aidan Feldman <aidan.feldman@gmail.com>
2022-10-19 01:45:31 -0400

    throw error if not all response mocks are used

--- tests/test_airtable.py
+++ tests/test_airtable.py
@@ -1,31 +1,40 @@
-import responses
 from esd_crawl import airtable
+import pytest
+import responses
 
 
-@responses.activate
-def test_create_record_success():
-    table_name = "Fake"
-    response = responses.Response(
-        method="POST",
-        url=f"https://api.airtable.com/v0/appdrqXSd2JNXkLp7/{table_name}",
+@pytest.fixture
+def r_mock():
+    # https://github.com/getsentry/responses#requestmock-methods-start-stop-reset
+    mock = responses.RequestsMock(assert_all_requests_are_fired=True)
+    mock.start()
+    yield mock
+    mock.stop()
+    mock.reset()
+
+
+def mock_create_record(r_mock, table_name):
+    r_mock.post(
+        f"https://api.airtable.com/v0/appdrqXSd2JNXkLp7/{table_name}",
         json={"records": [{"id": "456"}]},
     )
-    responses.add(response)
+
+
+def test_create_record_success(r_mock):
+    table_name = "Fake"
+    mock_create_record(r_mock, table_name)
 
     id = airtable.create_record("123", table_name, {})
     assert id is not None
 
 
-@responses.activate
-def test_create_record_fail():
+def test_create_record_fail(r_mock):
     table_name = "Fake"
-    response = responses.Response(
-        method="POST",
-        url=f"https://api.airtable.com/v0/appdrqXSd2JNXkLp7/{table_name}",
+    r_mock.post(
+        f"https://api.airtable.com/v0/appdrqXSd2JNXkLp7/{table_name}",
         json={},
         status=404,
     )
-    responses.add(response)
 
     id = airtable.create_record("123", table_name, {})
     assert id is None
