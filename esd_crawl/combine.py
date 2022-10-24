7ac164bac76a0f21c7ff7fcea98150bbef8e0966
Aidan Feldman <aidan.feldman@gmail.com>
2022-10-24 17:05:39 -0400

    use PDF class throughout combine script

--- esd_crawl/combine.py
+++ esd_crawl/combine.py
@@ -1,4 +1,5 @@
 import csv
+from esd_crawl.items import PDF
 import json
 
 ORIGIN = "https://esd.ny.gov"
@@ -8,7 +9,12 @@ class PdfSet:
     def __init__(self):
         self.pdfs = {}
 
-    def add(self, url: str, title: str, source: str, img_paths: list[str]):
+    def add(self, pdf: PDF):
+        if len(pdf.file_urls) > 1:
+            raise RuntimeError("PDF has more than one file URL")
+
+        url = pdf.file_urls[0]
+
         if url.startswith("/"):
             # use absolute URLs
             url = ORIGIN + url
@@ -22,9 +28,9 @@ class PdfSet:
             }
 
         info = self.pdfs[url]
-        info["titles"].add(title.strip())
-        info["sources"].add(source)
-        info["img_paths"].update(img_paths)
+        info["titles"].add(pdf.title.strip())
+        info["sources"].add(pdf.source)
+        info["img_paths"].update(pdf.img_paths)
 
     def to_dict(self):
         return self.pdfs
@@ -44,8 +50,8 @@ def run():
     with open("scrapy.json") as file:
         data = json.load(file)
         for entry in data:
-            url = entry["file_urls"][0]
-            pdfs.add(url, entry["title"], entry["source"], entry["img_paths"])
+            pdf = PDF(**entry)
+            pdfs.add(pdf)
 
     with open("parsehub.csv") as file:
         reader = csv.DictReader(file)
@@ -54,7 +60,13 @@ def run():
 
             # The report will contain links to both PDFs and other pages. The latter will be covered by the sitemap crawl, so we can ignore them here.
             if url.endswith(".pdf"):
-                pdfs.add(url, entry["report_name"], entry["report_source"], [])
+                pdf = PDF(
+                    title=entry["report_name"],
+                    source=entry["report_source"],
+                    file_urls=[url],
+                    img_paths=[],
+                )
+                pdfs.add(pdf)
 
     with open("pdfs.json", "w") as file:
         json.dump(pdfs.to_dict(), file, cls=SetEncoder)
--- tests/test_combine.py
+++ tests/test_combine.py
@@ -1,25 +1,38 @@
 from esd_crawl.combine import PdfSet
+from esd_crawl.items import PDF
 
 
 def test_duplicates():
     pdf_set = PdfSet()
-    pdf_set.add(
-        "http://foo.com/test1.pdf", "Test PDF 1", "https://foo.com/", ["123.png"]
+
+    pdf1 = PDF(
+        title="Test PDF 1",
+        source="https://foo.com/",
+        file_urls=["http://foo.com/test1.pdf"],
+        img_paths=["123.png"],
     )
-    pdf_set.add(
-        "http://foo.com/test2.pdf", "Test PDF 2", "https://foo.com/", ["456.png"]
+    pdf_set.add(pdf1)
+
+    pdf2 = PDF(
+        title="Test PDF 2",
+        source="https://foo.com/",
+        file_urls=["http://foo.com/test2.pdf"],
+        img_paths=["456.png"],
     )
-    pdf_set.add(
-        "http://foo.com/test1.pdf",
-        "Test PDF 1 - other",
-        "https://foo.com/",
-        ["123.png"],
+    pdf_set.add(pdf2)
+
+    pdf3 = PDF(
+        title="Test PDF 1 - other",
+        source="https://foo.com/",
+        file_urls=["http://foo.com/test1.pdf"],
+        img_paths=["123.png"],
     )
+    pdf_set.add(pdf3)
 
     pdfs = pdf_set.to_dict()
     assert len(pdfs) == 2
 
-    pdf1 = pdfs["http://foo.com/test1.pdf"]
-    assert pdf1["titles"] == {"Test PDF 1", "Test PDF 1 - other"}
-    assert pdf1["sources"] == {"https://foo.com/"}
-    assert pdf1["img_paths"] == {"123.png"}
+    pdf_result = pdfs["http://foo.com/test1.pdf"]
+    assert pdf_result["titles"] == {"Test PDF 1", "Test PDF 1 - other"}
+    assert pdf_result["sources"] == {"https://foo.com/"}
+    assert pdf_result["img_paths"] == {"123.png"}
