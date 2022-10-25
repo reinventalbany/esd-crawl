aca31fe30e55b59cc6c14e2092e81f7d91d20b14
Aidan Feldman <aidan.feldman@gmail.com>
2022-10-25 02:45:08 -0400

    refactor parsehub script

--- esd_crawl/parsehub.py
+++ esd_crawl/parsehub.py
@@ -9,40 +9,52 @@ from scrapy.pipelines.media import MediaPipeline
 import sys
 
 
-def get_pdf(finder: TableFinder, entry: dict):
-    url = entry["report_url"]
+def get_pdf(finder: TableFinder, report: dict):
+    url = report["report_url"]
 
     info = MediaPipeline.SpiderInfo(None)
     tables = finder.find_tables_from_url(url, info)
 
     return PDF(
-        title=entry["report_name"],
-        source=entry["report_source"],
+        title=report["report_name"],
+        source=report["report_source"],
         file_urls=[url],
         tables=tables,
     )
 
 
-def run():
+def process_report(finder: TableFinder, report: dict):
+    url = report["report_url"]
+
+    # The report will contain links to both PDFs and other pages. The latter will be covered by the sitemap crawl, so we can ignore them here.
+    if url.endswith(".pdf"):
+        try:
+            return get_pdf(finder, report)
+        except PDFSyntaxError:
+            print("\nPDF could not be processed:", url, file=sys.stderr)
+            return None
+
+    return None
+
+
+def get_pdfs(parsehub_output_csv: str):
     finder = TableFinder()
     pdfs = []
 
-    with open("parsehub.csv") as file:
+    with open(parsehub_output_csv) as file:
         reader = csv.DictReader(file)
-        for entry in reader:
-            url = entry["report_url"]
-
-            # The report will contain links to both PDFs and other pages. The latter will be covered by the sitemap crawl, so we can ignore them here.
-            if url.endswith(".pdf"):
-                try:
-                    pdf = get_pdf(finder, entry)
-                except PDFSyntaxError:
-                    print("\nPDF could not be processed:", url, file=sys.stderr)
-                else:
-                    pdfs.append(pdf)
+        for report in reader:
+            pdf = process_report(finder, report)
+            if pdf:
+                pdfs.append(pdf)
 
             print(".", end="", flush=True)
 
+    return pdfs
+
+
+def run():
+    pdfs = get_pdfs("parsehub.csv")
     with open("reports.json", "w") as file:
         json.dump(pdfs, file, cls=DataClassEncoder)
 
