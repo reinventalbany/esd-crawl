0a1ae3c0a58429cf8a27ab192c40ddc38f9f18fe
Aidan Feldman <aidan.feldman@gmail.com>
2022-10-25 02:40:38 -0400

    remove use of global variables in parsehub script

--- esd_crawl/parsehub.py
+++ esd_crawl/parsehub.py
@@ -9,7 +9,9 @@ from scrapy.pipelines.media import MediaPipeline
 import sys
 
 
-def get_pdf(url: str):
+def get_pdf(finder: TableFinder, entry: dict):
+    url = entry["report_url"]
+
     info = MediaPipeline.SpiderInfo(None)
     tables = finder.find_tables_from_url(url, info)
 
@@ -21,24 +23,29 @@ def get_pdf(url: str):
     )
 
 
-finder = TableFinder()
-pdfs = []
+def run():
+    finder = TableFinder()
+    pdfs = []
 
-with open("parsehub.csv") as file:
-    reader = csv.DictReader(file)
-    for entry in reader:
-        url = entry["report_url"]
+    with open("parsehub.csv") as file:
+        reader = csv.DictReader(file)
+        for entry in reader:
+            url = entry["report_url"]
 
-        # The report will contain links to both PDFs and other pages. The latter will be covered by the sitemap crawl, so we can ignore them here.
-        if url.endswith(".pdf"):
-            try:
-                pdf = get_pdf(url)
-            except PDFSyntaxError:
-                print("\nPDF could not be processed:", url, file=sys.stderr)
-            else:
-                pdfs.append(pdf)
+            # The report will contain links to both PDFs and other pages. The latter will be covered by the sitemap crawl, so we can ignore them here.
+            if url.endswith(".pdf"):
+                try:
+                    pdf = get_pdf(finder, entry)
+                except PDFSyntaxError:
+                    print("\nPDF could not be processed:", url, file=sys.stderr)
+                else:
+                    pdfs.append(pdf)
 
-        print(".", end="", flush=True)
+            print(".", end="", flush=True)
 
-with open("reports.json", "w") as file:
-    json.dump(pdfs, file, cls=DataClassEncoder)
+    with open("reports.json", "w") as file:
+        json.dump(pdfs, file, cls=DataClassEncoder)
+
+
+if __name__ == "__main__":
+    run()
