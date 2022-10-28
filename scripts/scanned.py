e41d0e2686d7b905799b0c807344dda16554e2d6
Aidan Feldman <aidan.feldman@gmail.com>
2022-10-28 11:10:21 -0400

    handle errors when checking scanned PDFs

--- scripts/scanned.py
+++ scripts/scanned.py
@@ -2,17 +2,24 @@
 
 from esd_crawl.pdf_lib import pdf_has_text
 from glob import glob
+import sys
 
 num_files = 0
 num_scanned = 0
 
 for filename in glob("downloads/full/*.pdf"):
     num_files += 1
-    if not pdf_has_text(filename):
+    try:
+        has_text = pdf_has_text(filename)
+    except Exception as e:
+        print(f"\nError parsing file {filename}:", file=sys.stderr)
+        print(e, file=sys.stderr)
+        continue
+
+    if not has_text:
         num_scanned += 1
 
     print(".", end="", flush=True)
 
-print(
-    f"\n{num_scanned} of {num_files} PDFs ({round(num_scanned / num_files)}) appear to be scanned."
-)
+pct = round(num_scanned / num_files)
+print(f"\n{num_scanned} of {num_files} PDFs ({pct}) appear to be scanned.")
