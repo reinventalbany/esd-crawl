54947428f9857d0d2c5c2cc549a864f00a799957
Aidan Feldman <aidan.feldman@gmail.com>
2022-11-17 10:27:14 -0500

    add test for a PDF link in the Broken spider

--- tests/spiders/test_broken.py
+++ tests/spiders/test_broken.py
@@ -2,15 +2,34 @@ from esd_crawl.spiders.broken import process_response
 from scrapy.http import Request, HtmlResponse
 
 
-def test_http_200():
-    body = b"""
+def html(body: str = ""):
+    html_str = f"""
     <!DOCTYPE html>
     <html>
     <head></head>
-    <body>
-    </body>
+    <body>{body}</body>
     </html>
     """
+
+    return bytes(html_str, "utf-8")
+
+
+def test_http_no_links():
+    response = HtmlResponse(
+        url="https://esd.ny.gov",
+        status=200,
+        headers={"Content-Type": "text/html"},
+        body=html(),
+        request=Request(url="https://esd.ny.gov"),
+    )
+
+    items = list(process_response(response))
+    assert items == []
+
+
+def test_http_pdf_link():
+    pdf_url = "https://esd.ny.gov/Reports/2015_2016/03312016_EXCELSIORJOBSPROGRAMQUARTERLYREPORT.pdf"
+    body = html(f"""<a href="{pdf_url}">PDF</a>""")
 
     response = HtmlResponse(
         url="https://esd.ny.gov",
@@ -20,5 +39,6 @@ def test_http_200():
         request=Request(url="https://esd.ny.gov"),
     )
 
-    items = process_response(response)
-    assert list(items) == []
+    items = list(process_response(response))
+    assert len(items) == 1
+    assert items[0].url == pdf_url
