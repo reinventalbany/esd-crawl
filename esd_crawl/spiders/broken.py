1181066b6183808ba89bf89c1ec9888cc3f4a8db
Aidan Feldman <aidan.feldman@gmail.com>
2022-11-17 11:13:27 -0500

    Broken: convert to SitemapSpider

--- esd_crawl/spiders/broken.py
+++ esd_crawl/spiders/broken.py
@@ -1,9 +1,7 @@
 from esd_crawl.items import BrokenLink
 from urllib.parse import urlparse
 from scrapy.http import Response, HtmlResponse
-
-# from scrapy.spiders import SitemapSpider
-from scrapy.spiders import Spider
+from scrapy.spiders import SitemapSpider
 
 
 def is_pdf_url(url: str):
@@ -17,8 +15,6 @@ def is_pdf(response: Response):
 
 
 def process_response(response: Response):
-    print("RESPONSE:", response)
-
     # broken PDF URLs redirect to the homepage
     if response.url == "https://esd.ny.gov":
         redirects = response.request.meta.get("redirect_urls")
@@ -42,22 +38,10 @@ def process_response(response: Response):
         #     yield response.follow(next_page, self.parse)
 
 
-# class BrokenSpider(SitemapSpider):
-#     name = "brokenspider"
-#     allowed_domains = ["esd.ny.gov"]
-#     sitemap_urls = ["https://esd.ny.gov/sitemap.xml"]
-class BrokenSpider(Spider):
+class BrokenSpider(SitemapSpider):
     name = "broken"
-    start_urls = [
-        "https://esd.ny.gov",
-        "https://esd.ny.gov/12312015-excelsior-jobs-program-quarterly-report",
-        "https://esd.ny.gov/broken.html",
-        "https://esd.ny.gov/broken.pdf",
-        "https://esd.ny.gov/Reports/2015_2016/03312016_EXCELSIORJOBSPROGRAMQUARTERLYREPORT.pdf",
-        "https://esd.ny.gov/Reports/2015_2016/EXCELSIORJOBSPROGRAMQUARTERLYREPORT_12312015.pdf",
-        "https://esd.ny.gov/Reports/2015_2016/RetentionChartA_BusinessesAdmittedToProgram_03312016.pdf",
-        "https://esd.ny.gov/Reports/2015_2016/RetentionChartB_CreditsIssued_03312016.pdf",
-    ]
+    allowed_domains = ["esd.ny.gov"]
+    sitemap_urls = ["https://esd.ny.gov/sitemap.xml"]
     # https://docs.scrapy.org/en/latest/topics/spider-middleware.html#module-scrapy.spidermiddlewares.httperror
     handle_httpstatus_list = [404]
 
